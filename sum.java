a206576a67fb33053e2f521f5161c390b888a23d
ash-511 <aishwarya51100@gmail.com>
2021-09-16 16:29:49 +0530

    Adding changes in file

--- sum.java
+++ sum.java
@@ -5,6 +5,6 @@ public static void main(String args[])
 int a,b;
 a=Integer.parseInt(args[0]);
 b=Integer.parseInt(args[1]);
-System.out.println("The sum of the entered two numbers is : " +(a+b));
+System.out.println("The sum of the two parameters is : " +(a+b));
 }
 }
